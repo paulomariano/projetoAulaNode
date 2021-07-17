cc08a29060aa0dc34fd6c2e4de00ee89c47161f7
paulomariano <paulo_fmariano@hotmail.com>
2021-07-17 11:52:05 -0300

    limpeza

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,13 +1,11 @@
 // var mysql = require('mysql')
-const { Client, Pool } = require('pg')
+const { Pool } = require('pg')
 const client = new Pool({
     host : 'tuffi.db.elephantsql.com',
     user : 'vxgqyvin',
     password : 'ZvurQU6qHZbsqWbAW4II01Qk0sTWSmoc',
     database : 'vxgqyvin'
 })
-
-
 
 // module.exports = function(){
 //     return pg.createConnection({
